97ec9010aab438b9d1dc368452976a294ebb3f1d
Lalitus <tallitosan@gmail.com>
2020-06-03 03:31:01 -0500

    added code to inserts 500 emails, and some queries

--- nodeIntro/app.js
+++ nodeIntro/app.js
@@ -8,20 +8,79 @@ const connection = mysql.createConnection({
   database : 'join_us'
 });
 
+// selecting data
 // let q = 'SELECT 1 + 1 AS solution';
 // let q = 'SELECT CURDATE()';
-let q = 'SELECT CURTIME() AS time, CURDATE() as date, NOW() AS now';
+// let q = 'SELECT CURTIME() AS time, CURDATE() as date, NOW() AS now';
+// let q = 'SELECT * FROM users';
+// let q = 'SELECT COUNT(*) AS total FROM users';
 
-connection.query(q, function(err, results, fields){
-    if(err){
-        throw err;
-    }
+// connection.query(q, function(err, results, fields){
+//   if(err){
+//     throw err;
+//   }
+//   console.log(results[0].total);
 
-    // console.log('The solution is: ', results[0].solution);
-    // console.log(results);//returns all the object
-    console.log(results[0].time);
-    console.log(results[0].date);
-    console.log(results[0].now);
+  // console.log('The solution is: ', results[0].solution);
+  // console.log(results);//returns all the object
+  // console.log(results[0].time);
+  // console.log(results[0].date);
+  // console.log(results[0].now);
+// });
+
+// inserting data
+// let person = {email:faker.internet.email};
+// let q = `INSERT INTO users (email) VALUES ("${email}")`;
+
+// connection.query('INSERT INTO users SET ?', person, function(err, results, fileds){
+//   if(err){
+//     throw err;
+//   }
+
+//   console.log(results)
+// });
+
+// insert data take 2
+// let person = {
+//   email:'Jenny 456@gmail.com',
+// };
+// let person = {
+//   email: faker.internet.email(),
+//   created_at: faker.date.past()
+// };
+
+// let end_result = connection.query('INSERT INTO users SET ?', person, function(err, result){
+//   if(err) throw err;
+//   console.log(result);
+// });
+
+// console.log(end_result);
+
+
+// this date format doesn`t work:
+// 2019-07-29T14:10:05.449Z
+// the library makes it work
+
+// bulk insert
+let data = [];
+for (let i = 0; i < 500; i++) {
+  data.push([
+    faker.internet.email(),
+    faker.date.past()
+  ]);
+}
+console.log(data);
+
+let q = 'INSERT INTO users (email, created_at) VALUES ?';
+
+connection.query(q, [data], function(err, result){
+  if(err){
+    console.log(err);
+    throw err;
+  }
+  console.log(result);
 });
 
 connection.end();
+
+// excercises
--- nodeIntro/joinUs.sql
+++ nodeIntro/joinUs.sql
@@ -1,4 +1,27 @@
 CREATE DATABASE join_us;
 USE join_us;
-SELECT CURDATE();
-SELECT 1+4;
+-- SELECT CURDATE();
+-- SELECT 1+4;
+
+CREATE TABLE users (
+    -- id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+-- exercises
+-- find earliest date a user joined (as earliest_date)
+-- with date correct format
+SELECT created_at AS earliest_date
+FROM users
+ORDER BY created_at DESC LIMIT 1;
+-- find email of the first earliest user
+
+-- Users according to the month they joined
+-- columns: month count
+
+-- Count number of users with yajoo emails
+SELECT COUNT(email)
+AS yahoo_users
+WHERE email
+LIKE '%yahoo.com'; 
