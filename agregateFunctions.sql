bab4e41d33e06bae3e7dc56637fa6a35f32bdf68
Lalitus <tallitosan@gmail.com>
2020-06-06 14:50:31 -0500

    end section

--- agregateFunctions.sql
+++ agregateFunctions.sql
@@ -111,3 +111,68 @@ GROUP BY author_lname, author_fname;
 SELECT author_fname, author_lname, SUM(released_year)
 FROM books
 GROUP BY author_lname, author_fname;
+
+--AVG
+-- Calculate te average released_year across all books
+SELECT AV(released_year) FROM books;
+SELECT AV(pages) FROM books;
+-- Calculate the average stock quantity for books released in the same year
+SELECT AVG(stock_quantity)
+FROM books
+GROUP BY released_year;
+--
+SELECT released_year, AVG(stock_quantity)
+FROM books
+GROUP BY released_year;
+--
+SELECT author_fname, author_lname, AVG(pages)
+FROM books
+GROUP BY author_fname, author_lname;
+
+--Examples
+-- Print the bumber of books in the database
+SELECT COUNT(*) AS 'How many' FROM books;
+-- How many books were released in each year
+SELECT 
+released_year AS Year,
+COUNT(*) AS 'How many'
+FROM books
+GROUP BY released_year;
+-- Print the total number of book in stock
+SELECT SUM(stock_quantity) AS stock
+FROM books;
+--Find the average released_year for each author
+SELECT
+CONCAT(
+    author_fname, ' ', author_lname
+) AS author,
+AVG(released_year) AS year
+FROM books
+GROUP BY author_fname, author_lname;
+-- Find the fullname of the author who wrote the longest book
+SELECT 
+CONCAT(
+    author_fname, ' ', author_lname
+) AS Author,
+MAX(pages) AS Pages
+FROM books
+GROUP BY pages
+ORDER BY  pages DESC LIMIT 1;
+-- 
+SELECT
+CONCAT(
+    author_fname, ' ', author_lname
+) AS Author
+pages AS Pages
+FROM books
+WHERE pages = (
+    SELECT MAX(pages)
+    FROM books
+);
+-- Make this happen
+SELECT
+released_year AS year,
+COUNT(*) AS '# books',
+AVG(pages) AS 'avg pages'
+FROM books
+GROUP BY released_year;
