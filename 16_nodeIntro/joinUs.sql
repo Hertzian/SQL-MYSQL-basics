b136b7b823c9fbcd245a8abfa91b5b51a35e3977
Hertzian <tallitosan@gmail.com>
2021-11-17 02:02:29 -0600

    done with node intro & joinUs DB

--- 16_nodeIntro/joinUs.sql
+++ 16_nodeIntro/joinUs.sql
@@ -10,18 +10,43 @@ CREATE TABLE users (
 );
 
 -- exercises
+-- Challenge 1
 -- find earliest date a user joined (as earliest_date)
 -- with date correct format
-SELECT created_at AS earliest_date
-FROM users
-ORDER BY created_at DESC LIMIT 1;
+SELECT 
+DATE_FORMAT(MIN(created_at), '%M %D %Y') AS earliest_date
+FROM users;
+
+-- Challenge 2
 -- find email of the first earliest user
+SELECT * FROM users
+WHERE created_at = (
+    SELECT MIN(created_at) FROM users
+);
 
+-- Challenge 3
 -- Users according to the month they joined
--- columns: month count
+SELECT MONTHNAME(created_at) AS month, COUNT(*) AS count
+FROM users
+GROUP BY month
+ORDER BY count DESC;
 
--- Count number of users with yajoo emails
-SELECT COUNT(email)
-AS yahoo_users
-WHERE email
-LIKE '%yahoo.com'; 
+-- Challenge 4
+-- Count number of users with yahoo emails
+SELECT COUNT(*) AS yahoo_users
+FROM users
+WHERE email LIKE '%@yahoo.com'; 
+
+-- Challenge 5
+-- Calculate total number of users for each email host
+SELECT
+    CASE
+        WHEN email LIKE '%@gmail.com' THEN 'gmail'
+        WHEN email LIKE '%@yahoo.com' THEN 'yahoo'
+        WHEN email LIKE '%@hotmail.com' THEN 'hotmail'
+        ELSE 'other'
+    END AS provider,
+    COUNT(*) AS total_users
+FROM users
+GROUP BY provider
+ORDER BY total_users DESC;
