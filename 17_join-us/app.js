75ec2874e90237e71e9205b1b2b1c1be8ec43fcc
Hertzian <tallitosan@gmail.com>
2021-11-19 18:54:09 -0600

    Join Us app done

--- 17_join-us/app.js
+++ 17_join-us/app.js
@@ -3,6 +3,10 @@ const mysql = require('mysql')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/public`))
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'lalo',
@@ -18,12 +22,29 @@ app.get('/', (req, res) => {
     //console.log(results[0])
     const count = results[0].count
     //Respond with that count
-    return res.send(`We have ${count} users in our DB`)
+    //return res.send(`We have ${count} users in our DB`)
+    //return res.render('home', { count: count, favColour: 'blue' })
+    return res.render('home', { count })
   })
 })
 
+app.post('/register', (req, res) => {
+  //const query = `INSERT INTO users(email) VALUES ('${req.body.email}')`
+  const person = { email: req.body.email }
+  connection.query('INSERT INTO users SET ?', person, (err, result) => {
+    if (err) throw err
+    return res.redirect('/')
+    //return res.send('Thanks for joining our wait list')
+  })
+  //connection.query(query, (err, results) => {
+  //if (err) throw err
+  //return res.redirect('/')
+  //})
+})
+
 app.get('/joke', (req, res) => {
-  const joke = 'What do youll a dog that does magic tricks? A labracadabrador.'
+  const joke =
+    '<strong>What do youll a dog that does magic tricks?</strong> <em>A labracadabrador.</em>'
   res.send(joke)
 })
 
